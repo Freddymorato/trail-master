1dbcb0f42fcf4bd7236a262774d01edd04895471
Freddymorato <Freddymorato@gmail.com>
2018-07-31 14:44:12 -0400

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "home#index"
+
+  resources :users, only: [:new, :create, :show] do
+    resources :trails
+  end
+
+  resources :countries, only: [:index, :show]
+
+  resources :trails, only: [:show]
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
+  get "auth/facebook/callback" =>  'sessions#facebook'
+
+  get '/trails_by_time' => 'trails#trails_by_time'
+  get '/trails_by_distance' => 'trails#trails_by_distance'
+  get '/trails_by_difficulty' => 'trails#trails_by_difficulty'
 end
