6cd551c49cf0e89511ef3f1b81206e196d9da8cb
Freddymorato <Freddymorato@gmail.com>
2018-11-28 19:08:56 -0500

    refactored and updated main JS file

--- app/assets/javascripts/trail_master.js
+++ app/assets/javascripts/trail_master.js
@@ -2,68 +2,84 @@
 let current_path = window.location.pathname;
 let user = current_path.substring(7).slice(0, 1);
 let trail = current_path.substring(current_path.lastIndexOf('/') + 1);
-let url = `/users/${user}/trails/${trail}/`;
+let url = `/users/${user}/trails/${trail}`;
 let auth_token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
 
-// ES6 class syntax
-function Comment(resp, user, trail) {
-  this.id = resp.id;
-  this.content = resp.content;
-  this.user_id = user;
-  this.trail_id = trail;
+class Comment {
+  constructor(resp) {
+    this.id = resp.id;
+    this.content = resp.content;
+    this.userId = resp.user_id;
+    this.trailId = resp.trail_id;
+    this.userName = resp.comment_owner;
+  };
+
+  // Show Comment
+  showComment() {
+    $("#comment_list").show();
+    $('div#com-section').empty();
+    $('div#com-section').append(
+      `<div class="comment-wrapper">
+      <small id="name-tag">${this.userName}</small><br><br>
+      <p class="commentid-${this.id}">${this.content}<br><br>
+      <button class="delete-commentid-${this.id}">Delete Comment</button>
+      </p></div>`);
+      $(".delete-commentid-"+this.id).on('click', () => this.deleteComment());
+  };
+
+  // Delete A Comment
+  deleteComment() {
+    if (this.userId == user) {
+      $.ajax({
+        type: 'DELETE',
+        url: url + '/comments/' + this.id + '.json',
+        data: {authenticity_token: auth_token},
+        success: function (data) {
+          $("#url" + this.id).remove();
+        },
+        error: function (data) {
+          console.error('Error:', data);
+        }
+      });
+      event.preventDefault();
+      alert("Comment successfully deleted.");
+    }
+    else {
+      alert("You must own the comment inorder to delete it.");
+    }
+    commentList();
+  };
 };
 
-// Object.prototype
-Comment.prototype.renderComment = function() {
-  showComment(this.id);
-};
-
-// Index of Comments
+// Comment Index
 function commentList() {
   $('div#com-section').empty();
-  $.get(url + 'comments.json', function (comments) {
-    let trailComments = comments.filter(comment => comment.trail_id == trail);
-    trailComments.forEach(function(comment) {
+  $.get(url + '.json', function (trailJson) {
+    trailJson.comments.forEach(function(commentJson) {
+      let comment = new Comment(commentJson);
       $('div#com-section').append(`<div class="comment-wrapper">
-      <small id="name-tag">${getUsername(comment)}</small><br><br>
+      <small id="name-tag">${comment.userName}</small><br><br>
       <p class="commentid-${comment.id}">${comment.content}</p></div>`);
-      $(".commentid-"+comment.id).on('click', () => showComment(comment.id));
+      $(".commentid-"+comment.id).on('click', () => comment.showComment());
     });
   });
   $("#comment_list").hide();
   $("#my_comments").show();
 };
 
-// Show Comment
-function showComment(commentid) {
-  $("#comment_list").show();
-  $.get(url + 'comments/' + commentid + '.json', function(data) {
-    $('div#com-section').empty();
-    let comment = data;
-    $('div#com-section').append(
-      `<div class="comment-wrapper">
-      <small id="name-tag">${getUsername(comment)}</small><br><br>
-      <p class="commentid-${comment.id}">${comment.content}<br><br>
-      <button class="delete-commentid-${comment.id}">Delete Comment</button>
-      </p></div>`
-    );
-    $(".delete-commentid-"+comment.id).on('click', () => deleteComment(comment));
-  });
-};
-
-// Show Logged In User's Comments For Current Trail
+// Show current User's Comments for Trail
 function myComments(user) {
   $("#comment_list").show();
   $('div#com-section').empty();
-  $.get(url + 'comments.json', function (comments) {
-  let trailComments = comments.filter(comment => comment.trail_id == trail);
-  let userComments = trailComments.filter(comment => comment.user_id == user);
-  userComments.forEach(function(comment) {
+  $.get(url + '.json', function (trailJson) {
+    let userComments = trailJson.comments.filter(comment => comment.user_id == user);
+    userComments.forEach(function(userComment) {
+      let comment = new Comment(userComment);
       $('div#com-section').append(`<div class="comment-wrapper">
-      <small id="name-tag">${getUsername(comment)}</small><br><br>
+      <small id="name-tag">${comment.userName}</small><br><br>
       <p class="commentid-${comment.id}">${comment.content}</p></div>`);
-      $(".commentid-"+comment.id).on('click', () => showComment(comment.id));
+      $(".commentid-"+comment.id).on('click', () => comment.showComment());
     });
   });
   $("#my_comments").hide();
@@ -77,51 +93,25 @@ $(function() {
     event.preventDefault();
     $.ajax({
       method: 'POST',
-      url: url + 'comments.json',
+      url: url + '/comments.json',
       data: $(this).serialize(), // serializes form data
       success: function(resp) {
         // on success update DOM with resp in the form of data
-        let comment = new Comment(resp, user, trail);
-        comment.renderComment();
+        let comment = new Comment(resp);
+        comment.showComment();
       }
     });
     $("#new_comment").trigger('reset');
   });
 });
 
- // Delete A Comment
-function deleteComment(comment) {
-  if (comment.user_id == user) {
-    $.ajax({
-      type: 'DELETE',
-      url: url + 'comments/' + comment.id + '.json',
-      data: {authenticity_token: auth_token},
-      success: function (data) {
-        $("#url" + comment.id).remove();
-      },
-      error: function (data) {
-        console.error('Error:', data);
-      }
-    });
-    event.preventDefault();
-    alert("Comment successfully deleted.");
-  }
-  else {
-    alert("You must own the comment inorder to delete it.");
-  }
-  commentList();
-};
 
-// Get Comment Owner's Name
-function getUsername(comment) {
-  return comment.user.name;
-};
 
 function attachListeners () {
   $("#comment_list").on('click', () => commentList());
   $("#my_comments").on('click', () => myComments(user));
 };
 
-$(document).ready(() => {
+$(document).on("turbolinks:load", () => {
   attachListeners();
 });
